cfb69369c2d1132659bebc61b8a3f6e0ed58efbf
soniagkalliath <soniageorge111@gmail.com>
2024-01-04 10:20:35 +0530

    counter app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './Componets/Counter'
+
+function App() {
+
+  return (
+    <div style={{width:'100%',height:'100vh'}} className='d-flex justify-content-center align-items-center'>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
